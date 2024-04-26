fbb5ec0c53708209625373f939b9ccb6c24cd8d7
Derrick Brundage <derrick.brundage@gmail.com>
2024-04-26 14:51:20 -0400

    Trying stuff

--- src/test/java/acceptance/StepDefinitions.java
+++ src/test/java/acceptance/StepDefinitions.java
@@ -2,6 +2,8 @@ package acceptance;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.en.Given;
+
 import org.springframework.web.client.RestTemplate;
 import static org.junit.Assert.assertEquals;
 /** Steps definitions for calculator.feature */
@@ -11,16 +13,19 @@ public class StepDefinitions {
     private String a;
     private String b;
     private String result;
+
     @Given("^I have two numbers: (.*) and (.*)$")
     public void i_have_two_numbers(String a, String b) throws Throwable {
         this.a = a;
         this.b = b;
     }
+
     @When("^the calculator sums them$")
     public void the_calculator_sums_them() throws Throwable {
         String url = String.format("%s/sum?a=%s&b=%s", server, a, b);
         result = restTemplate.getForObject(url, String.class);
     }
+
     @Then("^I receive (.*) as a result$")
     public void i_receive_as_a_result(String expectedResult) throws Throwable {
         assertEquals(expectedResult, result);
