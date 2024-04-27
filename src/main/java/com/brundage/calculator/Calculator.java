41c04ed765c0ebbb04124a4f64aa590c18ff398f
brundaged <derrick.brundage@gmail.com>
2024-04-27 03:05:27 -0400

    Updated referenced import libraries in test class

--- src/main/java/com/brundage/calculator/Calculator.java
+++ src/main/java/com/brundage/calculator/Calculator.java
@@ -2,12 +2,15 @@ package com.brundage.calculator;
 import org.springframework.stereotype.Service;
 @Service
 public class Calculator {
+
     public int sum(int a, int b) {
         return a + b;
     }
+
     public int product(int a, int b) {
         return a * b;
     }
+    
     public int difference(int a, int b) {
         return a - b;
     }
